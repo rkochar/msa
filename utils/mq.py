c8b994144bae0d4d5a1cec15a84cceb71aa6c53b
rkochar <rkochar9@gmail.com>
2024-01-30 11:13:45 +0100

    add more docs

--- utils/mq.py
+++ utils/mq.py
@@ -8,8 +8,7 @@ def create_mq():
 
     sqs_transaction, environment = m.create_message_queue(topic_name='transaction', environment=environment)
 
-    mq_lambda_iam_role = m.create_iam("apigw-lambda-iam-role", "lambda-basic-role",
-                                      "lambda-role-attachment", "sns-policy", "message-queue-policy")
+    mq_lambda_iam_role = m.create_iam("apigw-lambda-iam-role", "lambda-basic-role", "lambda-role-attachment", "sqs-policy", "message-queue-policy")
 
     lambda_mq_foo = m.create_lambda("mq", 'lambda-mq', "mq.print_message", role=mq_lambda_iam_role, environment=environment,
                                     template="mq", mq_topic=sqs_transaction,
--- utils/mvccdb.py
+++ utils/mvccdb.py
@@ -7,15 +7,15 @@ def create_mvccdb():
 
     sqldb, sqldb_lambda_environment = m.create_sql_database("sqldb", "mysql", "8.0.34", 10, "foouser", "foopass123", "small")
 
-    apigw_lambda_iam_role = m.create_iam("a-lambda-iam-role", "lambda-basic-role", "sql-attachment", "sql-policy", "mq-sql-policy")
-    sql_init_lambda = m.create_lambda('sqldb-init', "mvccdb/init", "mvcc_sqldb_init.sqldb_init", template="http_sql", environment=sqldb_lambda_environment, role=apigw_lambda_iam_role, imports=["pymysql"], time=False, span=False, opts=ResourceOptions(depends_on=[sqldb]))
-    sql_get_lambda = m.create_lambda('sqldb-get', "mvccdb/get", "mvcc_sqldb_get.sqldb_get", template="http_sql", environment=sqldb_lambda_environment, role=apigw_lambda_iam_role, imports=["pymysql", "pydantic"], time=True, span=False, opts=ResourceOptions(depends_on=[sqldb]))
+    sql_lambda_iam_role = m.create_iam("a-lambda-iam-role", "lambda-basic-role", "sql-attachment", "sql-policy", "mq-sql-policy")
+    sql_init_lambda = m.create_lambda('sqldb-init', "mvccdb/init", "mvcc_sqldb_init.sqldb_init", template="http_sql", environment=sqldb_lambda_environment, role=sql_lambda_iam_role, imports=["pymysql"], is_timed=False, span=False, opts=ResourceOptions(depends_on=[sqldb]))
+    sql_get_lambda = m.create_lambda('sqldb-get', "mvccdb/get", "mvcc_sqldb_get.sqldb_get", template="http_sql", environment=sqldb_lambda_environment, role=sql_lambda_iam_role, imports=["pymysql", "pydantic"], is_timed=True, span=False, opts=ResourceOptions(depends_on=[sqldb]))
 
     # Worker
     transaction_mq, worker_environment = m.create_message_queue(topic_name='transaction')
     mq_sql_lambda_iam_role = m.create_iam("apigw-lambda-iam-role", "lambda-basic-role", "mq-sql-attachment", "mq-sql-policy", "mq-sql-policy")
-    worker_lambda = m.create_lambda('worker', "mvccdb/worker", "worker.check_transaction", template="http_sql_pub",  environment=worker_environment | sqldb_lambda_environment, role=mq_sql_lambda_iam_role, imports=["pymysql", "pydantic"], time=True, span=False, opts=ResourceOptions(depends_on=[sqldb, transaction_mq]))
-    control_lambda = m.create_lambda('control', "mvccdb/control", "control.confirm_transaction", template="mq_sql", role=mq_sql_lambda_iam_role, mq_topic=transaction_mq, environment=worker_environment | sqldb_lambda_environment, imports=["pymysql"], time=False, span=True, opts=ResourceOptions(depends_on=[transaction_mq, sqldb, worker_lambda]))
+    worker_lambda = m.create_lambda('worker', "mvccdb/worker", "worker.check_transaction", template="http_sql_pub",  environment=worker_environment | sqldb_lambda_environment, role=mq_sql_lambda_iam_role, imports=["pymysql", "pydantic"], is_timed=True, span=False, opts=ResourceOptions(depends_on=[sqldb, transaction_mq]))
+    control_lambda = m.create_lambda('control', "mvccdb/control", "control.confirm_transaction", template="mq_sql", role=mq_sql_lambda_iam_role, mq_topic=transaction_mq, environment=worker_environment | sqldb_lambda_environment, imports=["pymysql"], is_timed=False, span=True, opts=ResourceOptions(depends_on=[transaction_mq, sqldb, worker_lambda]))
 
     routes = [
         ("/init", "GET", sql_init_lambda, "sqldb-init", "Setup database or MVCC-DB"),
